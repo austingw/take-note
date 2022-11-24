2f5211313cccf2ba3ce2cedf122e298accbdee74
austingw <senortinman@gmail.com>
2022-11-24 14:54:01 -0500

    Add title and tag form fields to NoteList component

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,3 +1,65 @@
-export default function NoteList() {
-  return <h1>List</h1>;
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { useState } from "react";
+import { Tag } from "./App";
+
+type NoteListProps = {
+  availableTags: Tag[];
+};
+
+export default function NoteList({ availableTags }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                isMulti
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(selectedTags) => {
+                  setSelectedTags(
+                    selectedTags.map((tag) => {
+                      return {
+                        label: tag.label,
+                        id: tag.value,
+                      };
+                    })
+                  );
+                }}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
