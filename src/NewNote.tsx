385bfdae9f2b9465ecfed572f5b2b674878f68aa
austingw <senortinman@gmail.com>
2022-11-24 14:13:25 -0500

    Add onSubmit function to App and connect to NewNote component as props

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,10 +1,14 @@
+import { NoteData } from "./App";
 import NoteForm from "./NoteForm";
 
-export default function NewNote() {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export default function NewNote({ onSubmit }: NewNoteProps) {
   return (
     <div>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm />
     </div>
   );
 }
