a84ffd43e433ebe2c8dce61daa389d5c33df0260
austingw <senortinman@gmail.com>
2022-11-25 12:01:56 -0500

    Fix tag display showing up in the middle of the page

--- src/NoteDisplay.tsx
+++ src/NoteDisplay.tsx
@@ -12,11 +12,7 @@ export default function NoteDisplay() {
         <Col>
           <h1>{note.title}</h1>
           {note.tags.length > 0 && (
-            <Stack
-              gap={1}
-              direction="horizontal"
-              className="justify-content-center flex-wrap"
-            >
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
               {note.tags.map((tag) => (
                 <Badge className="text-truncate" key={tag.id}>
                   {tag.label}
